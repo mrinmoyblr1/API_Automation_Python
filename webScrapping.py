fe501579f83ac9822f783e5b06977fdab9c9921f
mrinmoyblr1 <mrinmoy.blr@gmail.com>
2025-10-29 00:34:56 -0400

    SDFG

--- webScrapping.py
+++ webScrapping.py
@@ -1,7 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-data = requests.get("https://www.imdb.com/find/?s=ep&q=Thriller&ref_=hm_nv_srb_sm")
+data = requests.get("https://www.imdb.com/find/?s=ep&q=Thriller&ref_=hm_nv_srb_sm/")
 soup = BeautifulSoup(data.content, 'html.parser')
 
-print(soup.prettify())
+# print(soup.prettify())
+
+movieTable = soup.find('table', {'class': 'findList'})
+
+print(movieTable.prettify())
+
+rows = movieTable.findAll('tr')
+for row in rows:
+    tds = row.findAll('td')
+    print(tds)
